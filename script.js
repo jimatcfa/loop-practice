9e48f9bfcb0291a15b6bedba9111734906837bc0
jimboweb <jimboweb@gmail.com>
2022-07-18 15:52:24 -0700

    foreach loop

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let foods = ["pizza", "corn", "hot dogs", "ramen"];
-for(let food of foods){
+// for(let food of foods){
+//     console.log("I like to eat", food);
+// }
+
+let printFood = function(food){
     console.log("I like to eat", food);
 }
+
+foods.forEach(printFood);
